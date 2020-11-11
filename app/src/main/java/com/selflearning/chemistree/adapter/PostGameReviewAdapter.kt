9de95e71a233086c5f0818fb8588549870e196e3
Valeriy Sekirin <sekirim.valeriy@mail.ru>
2020-11-12 00:21:59 +0300

    3

---
+++ app/src/main/java/com/selflearning/chemistree/adapter/PostGameReviewAdapter.kt
@@ -0,0 +1,35 @@
+package com.selflearning.chemistree.adapter
+
+import android.text.Html
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.core.text.HtmlCompat
+import androidx.recyclerview.widget.RecyclerView
+import com.selflearning.chemistree.adapter.PostGameReviewAdapter.PostGameReviewViewHolder
+import com.selflearning.chemistree.databinding.RvPostGameReportBinding
+import com.selflearning.chemistree.models.ReportModels
+import java.util.*
+
+class PostGameReviewAdapter(private val arrayList: ArrayList<ReportModels>) : RecyclerView.Adapter<PostGameReviewViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PostGameReviewViewHolder {
+        val binding = RvPostGameReportBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return PostGameReviewViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: PostGameReviewViewHolder, position: Int) {
+        val reportModels = arrayList[position]
+        holder.bind(reportModels)
+    }
+
+    override fun getItemCount(): Int {
+        return arrayList.size
+    }
+
+    inner class PostGameReviewViewHolder(private val binding: RvPostGameReportBinding) : RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(model: ReportModels){
+            binding.tvReportDesc.text = HtmlCompat.fromHtml(model.correctDesc, HtmlCompat.FROM_HTML_MODE_LEGACY)
+            binding.tvReportName.text = Html.fromHtml(model.correctName)
+        }
+    }
+}
